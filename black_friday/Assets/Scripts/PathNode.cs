c145984b3a253d0dd0bda54d784a4691944e2c06
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-11-30 16:11:09 -0500

    Added A* pathfinding example with two pathfinders and five nodes

---
+++ black_friday/Assets/Scripts/PathNode.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PathNode : MonoBehaviour, IComparable<PathNode>
+{
+    //Total distance
+    public float F { get; set; }
+
+    //Actual distance from start
+    public float G { get; set; }
+
+    //Hueristic distance to end
+    public float H { get; set; }
+
+    //Parent vertex on the path per Pathfinder ID
+    public Dictionary<int, PathNode> Parent = new Dictionary<int,PathNode>();
+
+    //Node Placement in Lists per Pathfinder ID
+    public Dictionary<int, bool> InOpenList = new Dictionary<int,bool>();
+    public Dictionary<int, bool> InClosedList = new Dictionary<int,bool>();
+
+    // List of Adjacent Vertices
+    public List<PathNode> AdjacentVertices;
+
+    // Compares the distance that nodes take up on the path.
+    public int CompareTo(PathNode o)
+    {
+        return (int)(F - o.F);
+    }
+
+    // Allows for PathNodes to be implicitly casted as Vector3s that represent position
+    public static implicit operator Vector3(PathNode n)
+    {
+        return n.transform.position;
+    }
+
+    #region GraphBuilder
+
+    // A static list of all pathnodes that exist
+    public static List<PathNode> pathNodes = new List<PathNode>();
+    
+    // On Awake Add PathNode to static list
+    public void Awake()
+    {
+        pathNodes.Add(this);
+    }
+
+    // Add a pathfinder to all nodes and set default values
+    // inside all dictionaries for that Pathfinder ID
+    public static void AddPathfinder(int pathfinder)
+    {
+        for (int i = 0; i < pathNodes.Count; i++)
+        {
+            pathNodes[i].InClosedList[pathfinder] = false;
+            pathNodes[i].InOpenList[pathfinder] = false;
+            pathNodes[i].Parent[pathfinder] = null;
+        }
+    }
+
+    #endregion
+}
---
+++ black_friday/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AStarPathfinding
+{
+    class Pathfinder : MonoBehaviour
+    {
+        // The unique identifier for each pathfinder
+        private int id;
+
+        /// <summary>
+        /// All verticies that are left to be checked
+        /// </summary>
+        PriorityQueue<PathNode> openSet;
+
+        /// <summary>
+        /// All verticies that have already been checked
+        /// </summary>
+        List<PathNode> closedSet;
+
+        /// <summary>
+        /// A use once variable for making sure the path is calculated
+        /// </summary>
+        private bool isPathCalculated = false;
+
+        // The start and end points on the graph for this pathfinder
+        public PathNode _start, _end;
+
+        // The next node the pathfinder will travel to
+        private PathNode nextNode;
+
+        // Get pathfinder ID
+        public void Awake()
+        {
+            id = this.GetHashCode();
+        }
+
+        // Add pathfinder to GraphPathNode internal data
+        public void Start()
+        {
+            PathNode.AddPathfinder(this.id);
+        }
+
+        public void Update()
+        {
+            // Onetime calculate the ideal path
+            if (!isPathCalculated)
+            {
+                isPathCalculated = true;
+                this.transform.position = _start.transform.position;
+                AStar(_start, _end);
+                nextNode = _start;
+            }
+
+            // If within range of target node
+            if (Vector3.Distance(this.transform.position, nextNode) < 1.5f)
+            {
+                // If it's not the last node
+                if (nextNode != _end)
+                {
+                    // Get the next node and set a random color
+                    nextNode = FindNextNodeInPath(_end, nextNode);
+                    this.renderer.material.color = new Color(Random.value, Random.value, Random.value);
+                }
+                else
+                {
+                    // It's the last node, end it with red!
+                    this.renderer.material.color = Color.red;
+                }
+            }
+
+            // Get the force direction in order to reach the next node; Apply the force
+            Vector3 steerDirection = nextNode - this.transform.position;
+            this.rigidbody.AddForce(steerDirection.normalized * 1);
+        }
+        /// <summary>
+        /// Calculates the quickest distance between two points (Not ideal because of the manhattan distance hueristic)
+        /// </summary>
+        /// <param name="startingVertexPos">The starting position</param>
+        /// <param name="goalVertexPos">The goal position</param>
+        public void AStar(PathNode start, PathNode end)
+        {
+            PathNode startingVertex = start;
+            PathNode goalVertex = end;
+
+
+            //Reset the the open and closed lists of verticies
+            //Add the starting vertex to the open list
+            openSet = new PriorityQueue<PathNode>();
+            openSet.Enqueue(startingVertex);
+
+            closedSet = new List<PathNode>();
+            PathNode currentVertex;
+
+            //Loop through the open set until we reach the goal vertex
+            while (openSet.Peek() != goalVertex)
+            {
+                //Set the current vertex to the first vertex in the priority queue
+                currentVertex = openSet.Dequeue();
+                //Move it to the closed list
+                closedSet.Add(currentVertex);
+                currentVertex.InOpenList[id] = false;
+                currentVertex.InClosedList[id] = true;
+
+
+                List<PathNode> neighbors = currentVertex.AdjacentVertices;
+                //for neighbors of current
+                for (int i = 0; i < neighbors.Count; i++)
+                {
+                    PathNode neighbor = neighbors[i];
+                    //Calculate the neighbors hueristic
+                    float H = Vector3.Distance(neighbor, goalVertex);
+                    //Math.Abs(neighbor.X - goalVertex.X) + Math.Abs(neighbor.Y - goalVertex.Y);
+
+                    //If it's in the closed list skip this neighbor
+                    if (neighbor.InClosedList[id])
+                    {
+                        continue;
+                    }
+                    //If it's not in the open list
+                    if (!neighbor.InOpenList[id])
+                    {
+                        //Make it in the open list
+                        neighbor.InOpenList[id] = true;
+                        neighbor.InClosedList[id] = false;
+                        //Set its parent to the current vertex
+                        neighbor.Parent[id] = currentVertex;
+                        //Calculate distances
+                        neighbor.G = currentVertex.G + 1;
+                        neighbor.H = H;
+                        neighbor.F = neighbor.G + neighbor.H;
+                        //Add it to the open list
+                        openSet.Enqueue(neighbor);
+                    }
+                    else
+                    {
+                        //If its not in the open list, but this path is more ideal
+                        if (currentVertex.G + 1 < neighbor.G)
+                        {
+                            //Change its parent and recalculate distances
+                            neighbor.Parent[id] = currentVertex;
+                            neighbor.G = currentVertex.G + 1;
+                            neighbor.F = neighbor.G + neighbor.H;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the next node in the path
+        /// </summary>
+        /// <param name="goal">The final node that we wish to traverse to</param>
+        /// <param name="currentNode">The node we are currently on (or just passed)</param>
+        /// <returns>The next node in the path</returns>
+        public PathNode FindNextNodeInPath(PathNode goal, PathNode currentNode)
+        {
+            PathNode closestNode = goal;
+            while (closestNode.Parent[id] != null && closestNode.Parent[id] != currentNode)
+            {
+                closestNode = closestNode.Parent[id];
+            }
+            return closestNode;
+        }
+    }
+}
