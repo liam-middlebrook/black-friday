e80aa32441233af2ba3d25d35828687174dbea7c
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-11-30 20:52:31 -0500

    Added base for GPU based flow fields

---
+++ black_friday/Assets/Scripts/FlowFieldActor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowFieldActor : MonoBehaviour
+{
+    FlowFieldManager manager;
+
+    float lastCall = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+        manager = GameObject.Find("FlowField").GetComponent<FlowFieldManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastCall > 0.05f)
+        {
+            lastCall = Time.time;
+            Vector2 position2D = new Vector2(this.transform.position.x, this.transform.position.z);
+            Vector2 force2D = manager.GetForce2D(position2D);
+            Debug.Log(force2D);
+            this.rigidbody.AddForce(new Vector3(force2D.x, 0, force2D.y));
+        }
+    }
+}
---
+++ black_friday/Assets/Scripts/FlowFieldManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowFieldManager : MonoBehaviour
+{
+    public ComputeShader fieldShader;
+
+    int kernel_InitFlowfield;
+    int kernel_ForceAtPosition;
+
+    ComputeBuffer outputForce;
+
+    public RenderTexture fieldTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        fieldTexture = new RenderTexture(512, 512, 1);
+        fieldTexture.enableRandomWrite = true;
+
+        kernel_InitFlowfield = fieldShader.FindKernel("InitFlowfield");
+        kernel_ForceAtPosition = fieldShader.FindKernel("ForceAtPosition");
+
+        fieldShader.SetTexture(kernel_InitFlowfield, "NewField", fieldTexture);
+
+        fieldShader.SetTexture(kernel_ForceAtPosition, "FlowField", fieldTexture);
+
+        fieldShader.SetFloats("FieldSize", VectorToFloatArray(new Vector2(fieldTexture.width, fieldTexture.height)));
+        fieldShader.SetFloat("positionRatio", 1.0f);
+
+        outputForce = new ComputeBuffer(1, 8);
+
+        fieldShader.SetBuffer(kernel_ForceAtPosition, "force", outputForce);
+
+
+        fieldShader.Dispatch(kernel_InitFlowfield, fieldTexture.width/8, fieldTexture.height/8, 1);
+    }
+
+    public void OnDisable()
+    {
+        outputForce.Release();
+    }
+    public Vector2 GetForce2D(Vector2 position)
+    {
+        fieldShader.SetFloats("position", VectorToFloatArray(position));
+        fieldShader.SetFloat("step", 0);
+        fieldShader.Dispatch(kernel_ForceAtPosition, 512, 1, 1);
+        fieldShader.SetFloat("step", 4);
+        fieldShader.Dispatch(kernel_ForceAtPosition, 1, 1, 1);
+
+        float[] forceVector = new float[2];
+        outputForce.GetData(forceVector);
+
+        return new Vector2(forceVector[0], forceVector[1]);
+    }
+    float[] VectorToFloatArray(Vector2 vector)
+    {
+        return new float[] { vector.x, vector.y };
+    }
+}
