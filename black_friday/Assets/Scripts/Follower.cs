447cfca0049843fcdb1fe59f9828654c0b22103a
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-03 15:48:20 -0500

    Added base classes for leader managing and following

---
+++ black_friday/Assets/Scripts/Follower.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour
+{
+    private LeaderManager leaderManager;
+    public float followSpeed = 5.0f;
+    
+    public float followRadius = 2.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        leaderManager = GameObject.Find("LeaderManager").GetComponent<LeaderManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forceVector;
+
+        forceVector = leaderManager.Leader.transform.position - this.transform.position;
+
+        float dist = forceVector.magnitude;
+
+        forceVector.Normalize();
+        
+        Vector3 forceDir = forceVector;
+
+        forceVector *= followSpeed;
+
+        if (dist <= followRadius)
+        {
+            forceVector -= forceDir * (followRadius / dist) * followSpeed;
+        }
+        this.rigidbody.AddForce(forceVector);
+    }
+}
---
+++ black_friday/Assets/Scripts/LeaderManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaderManager : MonoBehaviour
+{
+    public GameObject leader = null;
+
+    public GameObject Leader
+    {
+        get { return leader; }
+        set { leader = value; }
+    }
+}
