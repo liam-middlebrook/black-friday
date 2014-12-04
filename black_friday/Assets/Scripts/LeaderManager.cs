d35f0e72f3662e247e434814e4173cd04aba6559
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-03 22:25:38 -0500

    LeaderManager now selects a leader when a sizeable group is chosen

--- black_friday/Assets/Scripts/LeaderManager.cs
+++ black_friday/Assets/Scripts/LeaderManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LeaderManager : MonoBehaviour
 {
@@ -9,5 +10,25 @@ public class LeaderManager : MonoBehaviour
     {
         get { return leader; }
         set { leader = value; }
+    }
+
+    // A static list of all pathnodes that exist
+    public static List<Wander> wanderers = new List<Wander>();
+
+    public void AssignLeader(GameObject _leader)
+    {
+        leader = _leader;
+        leader.renderer.material.color = Color.green;
+        leader.GetComponent<Separation>().forceAmt *= 5;
+
+        for (int i = 0; i < wanderers.Count; i++)
+        {
+            if (wanderers[i].gameObject != leader)
+            {
+                wanderers[i].gameObject.AddComponent<Follower>();
+                Destroy(wanderers[i]);
+                wanderers.RemoveAt(i--);
+            }
+        }
     }
 }
