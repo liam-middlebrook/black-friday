c86ed9503941986a448d7690f67e619537d242e4
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-03 22:26:35 -0500

    Removed raycasting from wander and abstracted to it's own behavor

---
+++ black_friday/Assets/Scripts/RaycastAvoidWall.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastAvoidWall : MonoBehaviour
+{
+
+    public float rayDistance = 1.0f;
+    public float sideRayDistance = 0.75f;
+
+    public float forceAmt = 10.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 steeringForce = Vector3.zero;
+        Debug.DrawRay(this.transform.position, this.rigidbody.velocity.normalized * rayDistance, Color.red);
+
+        Debug.DrawRay(this.transform.position, Vector3.Project(this.rigidbody.velocity, new Vector3(rayDistance, 0, 0)).normalized * sideRayDistance, Color.green);
+        Debug.DrawRay(this.transform.position, Vector3.Project(this.rigidbody.velocity, new Vector3(0, 0, rayDistance)).normalized * sideRayDistance, Color.blue);
+
+        RaycastHit hit;
+        Vector3 leftRay = Vector3.Project(this.rigidbody.velocity, new Vector3(rayDistance, 0, 0));
+        Vector3 rightRay = Vector3.Project(this.rigidbody.velocity, new Vector3(0, 0, rayDistance));
+        if (Physics.Raycast(new Ray(this.transform.position, this.rigidbody.velocity), out hit, rayDistance) && hit.collider.gameObject.tag == "Wall")
+        {
+            //Debug.Log("Ray hit");
+            steeringForce -= this.rigidbody.velocity.normalized;
+        }
+        if (Physics.Raycast(new Ray(this.transform.position, leftRay), out hit, sideRayDistance) && hit.collider.gameObject.tag == "Wall")
+        {
+            //Debug.Log("Ray hit");
+            steeringForce -= leftRay.normalized;
+        }
+        if (Physics.Raycast(new Ray(this.transform.position, rightRay), out hit, sideRayDistance) && hit.collider.gameObject.tag == "Wall")
+        {
+            //Debug.Log("Ray hit");
+            steeringForce -= rightRay.normalized;
+        }
+        if (Physics.Raycast(new Ray(this.transform.position, -leftRay), out hit, sideRayDistance) && hit.collider.gameObject.tag == "Wall")
+        {
+            //Debug.Log("Ray hit");
+            steeringForce -= -leftRay.normalized;
+        }
+        if (Physics.Raycast(new Ray(this.transform.position, -rightRay), out hit, sideRayDistance) && hit.collider.gameObject.tag == "Wall")
+        {
+            //Debug.Log("Ray hit");
+            steeringForce -= -rightRay.normalized;
+        }
+
+        this.rigidbody.AddForce(steeringForce * forceAmt);
+    }
+}
--- black_friday/Assets/Scripts/Wander.cs
+++ black_friday/Assets/Scripts/Wander.cs
@@ -19,124 +19,139 @@ public class Wander : MonoBehaviour
     public float oppisiteDirection = 0.5f;
     public float orthoDirection = 0.75f;
 
-    public float rayDistance = 1.0f;
-    public float sideRayDistance = 0.75f;
 
     public float wanderSpeed = 10.0f;
+
+
+    public float leaderDist = 5.0f;
+    public int numForLeader = 3;
+
+    private float wanderTimer;
+    private float wanderTime = 0.1f;
     void Start()
     {
         directionValues[Direction.NORTH] = Vector3.forward;
         directionValues[Direction.SOUTH] = Vector3.back;
         directionValues[Direction.EAST] = Vector3.right;
         directionValues[Direction.WEST] = Vector3.left;
+
+        LeaderManager.wanderers.Add(this);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         Vector3 steeringForce = Vector3.zero;
-        switch (currentDirection)
-        {
-            case Direction.NORTH:
-                if (Random.value < sameDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.NORTH];
-                }
-                else if (Random.value < oppisiteDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.SOUTH];
-                }
-                else if (Random.value < orthoDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.EAST];
-                }
-                else
-                {
-                    steeringForce += directionValues[currentDirection = Direction.WEST];
-                }
-                break;
-            case Direction.SOUTH:
-                if (Random.value < sameDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.SOUTH];
-                }
-                else if (Random.value < oppisiteDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.NORTH];
-                }
-                else if (Random.value < orthoDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.EAST];
-                }
-                else
-                {
-                    steeringForce += directionValues[currentDirection = Direction.WEST];
-                }
-                break;
-            case Direction.EAST:
-                if (Random.value < sameDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.EAST];
-                }
-                else if (Random.value < oppisiteDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.WEST];
-                }
-                else if (Random.value < orthoDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.NORTH];
-                }
-                else
-                {
-                    steeringForce += directionValues[currentDirection = Direction.SOUTH];
-                }
-                break;
-            case Direction.WEST:
-                if (Random.value < sameDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.WEST];
-                }
-                else if (Random.value < oppisiteDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.EAST];
-                }
-                else if (Random.value < orthoDirection)
-                {
-                    steeringForce += directionValues[currentDirection = Direction.NORTH];
-                }
-                else
-                {
-                    steeringForce += directionValues[currentDirection = Direction.SOUTH];
-                }
-                break;
-            default:
-                break;
-        }
+        wanderTimer += Time.deltaTime;
 
+        if (wanderTimer >= wanderTime)
+        {
+            wanderTimer = 0.0f;
+
+            switch (currentDirection)
+            {
+                case Direction.NORTH:
+                    if (Random.value < sameDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.NORTH];
+                    }
+                    else if (Random.value < oppisiteDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.SOUTH];
+                    }
+                    else if (Random.value < orthoDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.EAST];
+                    }
+                    else
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.WEST];
+                    }
+                    break;
+                case Direction.SOUTH:
+                    if (Random.value < sameDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.SOUTH];
+                    }
+                    else if (Random.value < oppisiteDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.NORTH];
+                    }
+                    else if (Random.value < orthoDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.EAST];
+                    }
+                    else
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.WEST];
+                    }
+                    break;
+                case Direction.EAST:
+                    if (Random.value < sameDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.EAST];
+                    }
+                    else if (Random.value < oppisiteDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.WEST];
+                    }
+                    else if (Random.value < orthoDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.NORTH];
+                    }
+                    else
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.SOUTH];
+                    }
+                    break;
+                case Direction.WEST:
+                    if (Random.value < sameDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.WEST];
+                    }
+                    else if (Random.value < oppisiteDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.EAST];
+                    }
+                    else if (Random.value < orthoDirection)
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.NORTH];
+                    }
+                    else
+                    {
+                        steeringForce += directionValues[currentDirection = Direction.SOUTH];
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            steeringForce += directionValues[currentDirection];
+        }
+        
         steeringForce *= wanderSpeed;
-        Debug.DrawRay(this.transform.position, this.rigidbody.velocity.normalized * rayDistance, Color.red);
-
-        Debug.DrawRay(this.transform.position, Vector3.Project(this.rigidbody.velocity, new Vector3(rayDistance, 0, 0)).normalized * sideRayDistance, Color.green);
-        Debug.DrawRay(this.transform.position, Vector3.Project(this.rigidbody.velocity, new Vector3(0, 0, rayDistance)).normalized * sideRayDistance, Color.green);
-
-        RaycastHit hit;
-        Vector3 leftRay = Vector3.Project(this.rigidbody.velocity, new Vector3(rayDistance, 0, 0));
-        Vector3 rightRay = Vector3.Project(this.rigidbody.velocity, new Vector3(0, 0, rayDistance));
-        if (Physics.Raycast(new Ray(this.transform.position, this.rigidbody.velocity), out hit, rayDistance) && hit.collider.gameObject.tag == "Wall")
-        {
-            //Debug.Log("Ray hit");
-            steeringForce -= this.rigidbody.velocity.normalized;
-        }
-        if (Physics.Raycast(new Ray(this.transform.position, leftRay), out hit, sideRayDistance) && hit.collider.gameObject.tag == "Wall")
-        {
-            //Debug.Log("Ray hit");
-            steeringForce -= Vector3.Project(this.rigidbody.velocity, new Vector3(rayDistance, 0, 0)).normalized;
-        }
-        if (Physics.Raycast(new Ray(this.transform.position, rightRay), out hit, sideRayDistance) && hit.collider.gameObject.tag == "Wall")
-        {
-            //Debug.Log("Ray hit");
-            steeringForce -= Vector3.Project(this.rigidbody.velocity, new Vector3(0, 0, rayDistance)).normalized;
-        }
         this.rigidbody.AddForce(steeringForce);
+    }
+
+    public void FixedUpdate()
+    {
+        int leadCount = 0;
+        foreach (Wander w in LeaderManager.wanderers)
+        {
+            if (w.gameObject == this.gameObject)
+                continue;
+
+            if (Vector3.Distance(this.transform.position, w.transform.position) < leaderDist)
+            {
+                ++leadCount;
+            }
+        }
+
+        if (leadCount >= numForLeader)
+        {
+            GameObject.Find("LeaderManager").GetComponent<LeaderManager>().AssignLeader(this.gameObject);
+        }
     }
 }
