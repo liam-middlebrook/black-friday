97070f007dd9474ef4525964b204b5867f8ec74c
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-03 22:27:17 -0500

    Updated Scene. Added Separation script

---
+++ black_friday/Assets/Scripts/Separation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Separation : MonoBehaviour
+{
+    public float forceAmt = 2.0f;
+    // Use this for initialization
+    void Start()
+    {
+        SphereCollider trigger = this.gameObject.AddComponent<SphereCollider>();
+        trigger.isTrigger = true;
+
+        trigger.radius = 2.0f;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "Separate")
+            return;
+
+        Vector3 diff = other.transform.position - this.transform.position;
+        diff.Normalize();
+
+        other.rigidbody.AddForce(diff * forceAmt);
+        //this.rigidbody.AddForce(-diff * forceAmt);
+    }
+}
